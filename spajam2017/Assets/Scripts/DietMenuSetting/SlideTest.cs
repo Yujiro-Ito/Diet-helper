e1c872d833f1f7e30da44360ba44137686c78744
TomoeUeyama <kentomoyuki@yahoo.co.jp>
2017-06-17 17:37:12 +0900

    SlideAnimの追加

---
+++ spajam2017/Assets/Scripts/DietMenuSetting/SlideTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideTest : MonoBehaviour {
+
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+	}
+
+	public void slideInAnim(){
+		_animator.SetBool ("running",true);
+	}
+
+	public void slideOutAnim(){
+		_animator.SetBool ("runnning",false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ spajam2017/Assets/Scripts/DietMenuSetting/buttonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour {
+	GameObject _slideObj;
+	SlideTest _slideTest;
+	public string panelName;
+
+	// Use this for initialization
+	void Start () {
+		_slideObj = GameObject.Find (panelName);
+		_slideTest = _slideObj.GetComponent<SlideTest> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void ButtonPush(){
+		_slideTest.slideInAnim ();
+	}
+}
