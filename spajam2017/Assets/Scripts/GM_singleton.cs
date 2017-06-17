95644853d5d115d43df331238adf2058629637d1
TomoeUeyama <kentomoyuki@yahoo.co.jp>
2017-06-17 13:55:23 +0900

    ManuManagerのついか

---
+++ spajam2017/Assets/Scripts/GM_singleton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_singleton : MonoBehaviour {
+	
+	private static GameObject _singletonObject;
+	private static GM_singleton _singleton;
+
+	private GameObject SceneFadeObj;
+	public static GM_singleton Instance() {
+		if (_singleton == null) {
+			_singletonObject = new GameObject ("Singleton");
+			_singletonObject.AddComponent<GM_singleton> ();
+			_singleton = _singletonObject.GetComponent<GM_singleton> ();
+		}
+		return _singleton;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//SceneFadeObj = GameObject.Find ("transporter_01_damge2/[CameraRig]/Camera(head)/Camera(eye)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//クリア後タイトルにいく
+	public void GoTitleScene()
+	{
+		//Camera.main.GetComponent<SceneFade> ().LoadSceenWithFade ("Title");
+		//Application.LoadLevel("Title");
+	}
+	//クリアsceneにいく
+	public void GoClearScene()
+	{
+		//SceneFadeのFadeOutメソッドを呼び出したあとClearシーンに遷移する
+		//Camera.main.GetComponent<SceneFade> ().LoadSceenWithFade ("Clear");
+	}
+
+	//オーバsceneにいく
+	public void GoOverScene()
+	{
+		Application.LoadLevel("Title");
+		//Camera.main.GetComponent<SceneFade> ().LoadSceenWithFade ("Title");
+	}
+}
---
+++ spajam2017/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+	//---------Singleton------------//
+	private static GameObject _singletonObject;
+	private static MenuManager _singleton;
+	//------------------------------//
+
+	//Data
+	private string menuName;
+	public string MenuName{ get{ return menuName; } set{ menuName = value; }}
+
+	private int menuCal;
+	public int MenuCal{ get{return menuCal; } set{ menuCal = value; }}
+
+
+	public static MenuManager Instance(){
+		if (_singleton == null) {
+			_singletonObject = new GameObject("Singleton");
+			_singletonObject.AddComponent<MenuManager> ();
+			_singleton = _singletonObject.GetComponent<MenuManager> ();
+		}
+		return _singleton;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
