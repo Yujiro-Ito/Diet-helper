f0790b93020ac8c6af0c1efe677fc505ec6d7d86
Yujiro Ito <yujiro.bear@gmail.com>
2017-06-18 00:52:49 +0900

    入力フォームの追加（食べ物検索）

---
+++ spajam2017/Assets/Scripts/DietMenuSetting/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Node : MonoBehaviour {
+	public Text foodName;
+	public Text calorie;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AssignData(string foodname, int calorieNum){
+		foodName.text = foodname;
+		calorie.text = calorieNum + "kcal";
+	}
+}
